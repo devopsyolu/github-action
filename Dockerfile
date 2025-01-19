596aae7e90830ab56c65ad430e285fd5a4410881
devopsyolu <devopsyolu@gmail.com>
2025-01-19 23:34:04 +0100

    fix some typo

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 # Set the base image to use for subsequent instructions
-FROM alpine:3.21
+FROM ubuntu
 
 # Set the working directory inside the container
 WORKDIR /usr/src
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,13 @@
 #!/bin/sh -l
 
 # Use INPUT_<INPUT_NAME> to get the value of an input
-GREETING="Hello, $INPUT_WHO_TO_GREET!"
+GREETING="Hello, $INPUT_who-to-greet!"
 
 # Use workflow commands to do things like set debug messages
 echo "$GREETING"
 
 # Write outputs to the $GITHUB_OUTPUT file
+echo "$(date)"
 echo "time=$(date)" >>"$GITHUB_OUTPUT"
 
 exit 0
